efcc7017d427800d95c29abdc83cdebcb4bcda3a
paulvmking <paulvmking@gmail.com>
2021-02-19 15:34:23 +0000

    added error response to user

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -70,6 +70,9 @@ function fetchGitHubInformation(event) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
