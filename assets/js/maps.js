5851cc5c9682c66b79098a94f4149d157a244184
paulvmking <paulvmking@gmail.com>
2021-02-19 13:43:17 +0000

    added js file to tidy code

---
+++ assets/js/maps.js
@@ -0,0 +1,33 @@
+function initMap() {
+            var map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 3,
+                center: {
+                    lat: 46.619261,
+                    lng: -33.134766
+                }
+            });
+        
+            let labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+
+            let locations = [{
+                lat: 40.785091,
+                lng: -73.968285
+            }, {
+                lat: 41.084045,
+                lng: -73.874256
+            }, {
+                lat: 40.754932,
+                lng: -73.984016
+            }];
+
+            let markers = locations.map(function(location, i) {
+                return new google.maps.Marker({
+                    position: location,
+                    label: labels[i % labels.length]
+                });
+            });
+
+            let markerCluster = new MarkerClusterer(map, markers, {
+                imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+            });
+        }
